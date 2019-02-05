fca14cc94077a7e0d39026486b893bfceeac8b05
syedmeesamali <syedmeesamali@yahoo.com>
2019-02-05 10:17:47 +0400

    Arrays in java along with loops etc

--- 1_Intro/gpa.java
+++ 1_Intro/gpa.java
@@ -1,5 +1,15 @@
-class gpa{
+//scanner is used for the input from user
+import java.util.Scanner;
+
+public class gpa{
+   //Always included in all java programs 
    public static void main(String args[]) {
-   System.out.println("Hello java world");
-   }
+      int a = 0;
+      //One method of declaring arrays in java
+      int[] myArray = {1,3,5,7,9};
+      for (int b=0; b<4; b++)
+      {
+         System.out.println("Array[ " + b + " ] is " + myArray[b]);
+      }
+   } // end of main program
 }
