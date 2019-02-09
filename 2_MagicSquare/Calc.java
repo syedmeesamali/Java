b800fd9ba92a29c93c8a0271b3499b0d5253561b
syedmeesamali <syedmeesamali@yahoo.com>
2019-02-09 21:59:04 +0400

    Tutorial following from youtube

---
+++ 2_MagicSquare/Calc.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.Scanner;
+
+public class Calc{
+   public static void main(String args[]) {
+      int a = 55;
+      System.out.println("Variable a is " + a);
+   }
+
+}
