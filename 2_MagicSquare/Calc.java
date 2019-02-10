c7d577f98aa75da060ef35713ed3c219f6aa80df
syedmeesamali <syedmeesamali@yahoo.com>
2019-02-10 23:23:16 +0400

    Frequency of certain items

--- 2_MagicSquare/Calc.java
+++ 2_MagicSquare/Calc.java
@@ -1,10 +1,20 @@
 import java.lang.*;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Calc{
    public static void main(String args[]) {
-      int a = 55;
-      System.out.println("Variable a is " + a);
+   Random rand = new Random();
+   int freq[] = new int[7];
+   for (int roll=1; roll<1000; roll++) {
+      ++freq[1+rand.nextInt(6)];
+    }
+   System.out.println("Face \n Frequency");
+   for (int face=0; face<freq.length; face++) {
+      System.out.println(face + "\n" + freq[face]);
+    }
+
+   
    }
 
 }
