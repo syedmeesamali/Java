7e710ad659a7f6512d3ee8720a28f74c63c47a71
syedmeesamali <syedmeesamali@yahoo.com>
2019-02-19 12:33:10 +0400

    edx course folder and examples

---
+++ 4_edx/edx.java
@@ -0,0 +1,10 @@
+//This java file will be used to test and work on misc codes from edx course
+public class edx
+{
+   public static void main(String[] args)
+   {
+      int[] a = new int[5];
+      System.out.println("Length of array a is: " + a.length);   
+   }   
+
+}
