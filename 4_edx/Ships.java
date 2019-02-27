e3559f7f4e57804e3db985982cf33e05004a119f
syedmeesamali <syedmeesamali@yahoo.com>
2019-02-27 18:12:09 +0400

    Created boundaries for the ships game

---
+++ 4_edx/Ships.java
@@ -0,0 +1,22 @@
+//This java file will be used to test and work on misc codes from edx course
+import java.util.Arrays;
+
+public class Ships
+{
+   public static void main(String[] args)
+   {
+      int[] player1 = new int[10];
+      int[] player2 = new int[10];
+      System.out.println("   0 1 2 3 4 5 6 7 8 9");
+      for (int i=0; i<10; i++)
+      {
+         System.out.println(i + " |" + "                   | " + i);
+         for (int j=0; j<10; j++)
+         {
+            
+         }
+      }
+      System.out.println("   0 1 2 3 4 5 6 7 8 9");
+   }      
+  
+} //End of class
