a914bf3663ce97a236127bfe7cd36739f07016d3
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-10 22:38:22 +0400

    Started work on BMP decoder project

---
+++ 4_edx/byteReader.java
@@ -0,0 +1,15 @@
+//This java file will be used to test and work on misc codes from edx course
+public class byteReader
+{
+   public static void main(String[] args)
+   {
+      byte r[], g[], b[];
+      int entries;
+      InputStream is;
+      int curPos = 0;
+      int w, h;
+      byte[] byteData;
+      int[] intData;
+                  
+   }  
+} 
