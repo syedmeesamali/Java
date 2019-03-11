1cca7914d5bb40f668471f0a55a91af7ae08be7a
syedmeesamali <syedmeesamali@yahoo.com>
2019-03-11 17:48:57 +0400

    Reading and writing bytes - little buggy code

--- 4_edx/byteReader.java
+++ 4_edx/byteReader.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.*;
+
+
 //This java file will be used to test and work on misc codes from edx course
 public class byteReader
 {
@@ -5,18 +9,27 @@ public class byteReader
    {
       byte r[], g[], b[];
       int entries;
-      InputStream is;
       int curPos = 0;
       int w, h;
       byte[] byteData;
       int[] intData;
+      
       try 
       {
          FileInputStream in = new FileInputStream("textfile.txt");
-         FileOutputStream out = new FileOutputStream("new.txt");         
-      } catch (Exception e)
+         FileOutputStream out = new FileOutputStream("new.txt");
+
+         int c = in.read();
+         while ((c != -1 ))
+         {
+            out.write(c);
+         }
+      } catch (FileNotFoundException e)
       {
-         
+         e.printStackTrace();
+      } catch (IOException e)
+      {
+         e.printStackTrace();
       }
    }  
 } 
