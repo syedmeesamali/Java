1654d9bef1b440a5ef1d34b16c5047a6c1841977
syedmeesamali <syedmeesamali@yahoo.com>
2019-05-30 22:56:17 +0400

    Byte AND function

--- 6_ByteReader/byteReader.java
+++ 6_ByteReader/byteReader.java
@@ -11,15 +11,18 @@ public class byteReader
       byte[] byteData;
       String s = "abc";
       byte[] bt = s.getBytes();
-      
+      Byte c = 87 & 101;
+
       System.out.println(Integer.toBinaryString(bt[0]));
+      System.out.println(Integer.toBinaryString(87));
+      System.out.println(Integer.toBinaryString(101));
+      System.out.println(Integer.toBinaryString(c));
       
       try 
       {
          File file = new File("white.bmp");
          FileInputStream in = new FileInputStream(file);
          DataInputStream input = new DataInputStream(in);
-         int c;
          System.out.println("Successfully loaded the BMP file");
 
       } catch (FileNotFoundException e)
