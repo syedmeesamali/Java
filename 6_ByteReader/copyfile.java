b70a63cc1d5768f7e391eaabf00948cb452c70bc
syedmeesamali <syedmeesamali@yahoo.com>
2019-06-11 16:53:24 +0400

    copyfile comit - 1

---
+++ 6_ByteReader/copyfile.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+//This java program copies one file to another file. Both names are provided.
+//------------------------
+
+public class copyfile
+{
+/** Main method
+ * args[0] - original file
+ * args[1] - result file
+ */
+   public static void main(String[] args)
+   {
+
+      try 
+      {
+         
+      } catch (FileNotFoundException e)
+      {
+         e.printStackTrace();
+      } catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+   }   //End of main
+} //End of class
