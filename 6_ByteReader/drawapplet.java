3b31b5e5cc498ef60ef78117fd7279d723693df4
syedmeesamali <syedmeesamali@yahoo.com>
2019-06-17 23:52:48 +0400

    Simple applet to be part of HTML

---
+++ 6_ByteReader/drawapplet.java
@@ -0,0 +1,12 @@
+import java.applet.*;
+import java.awt.*;
+
+
+//Small applet program
+public class drawapplet extends Applet
+{
+   public void paint(Graphics g)
+   {
+      g.drawString("Welcome to Applet world", 150, 150);
+   }
+} //End of class
