b0eca5da0c594410dedaf72d1ff1a318b6ad157b
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-25 17:49:56 +0400

    Some java quiz practice

---
+++ 8_Misc/AsapJobsClass.java
@@ -0,0 +1,9 @@
+public class AsapJobsClass
+{
+    int i;
+    public static void main(String[] args) {
+        AsapJobsClass obj = new AsapJobsClass();
+        System.out.println(obj.i);
+    }
+}
+
