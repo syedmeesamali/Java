83afd6a080698d81ba5543170655b9a38ed62416
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-26 22:03:41 +0400

    Some java practice questions

--- 8_Misc/AsapJobsClass.java
+++
@@ -1,9 +0,0 @@
-public class AsapJobsClass
-{
-    int i;
-    public static void main(String[] args) {
-        AsapJobsClass obj = new AsapJobsClass();
-        System.out.println(obj.i);
-    }
-}
-
---
+++ 8_Misc/AsapJobsClass2.java
@@ -0,0 +1,11 @@
+public class AsapJobsClass2
+{
+    public static void main(String[] args) {
+        System.out.println(test());
+    }
+    static int test() //Won't compile if not made static
+    {
+        return 100;
+    }
+} //End of overall class file
+
