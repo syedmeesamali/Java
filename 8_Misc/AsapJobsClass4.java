62a8099a56e5182fc745bbe913c6111df1cb7f49
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-26 22:43:39 +0400

    Some more classes

---
+++ 8_Misc/AsapJobsClass4.java
@@ -0,0 +1,8 @@
+public class AsapJobsClass4
+{
+    int x = 10;
+    public static void main(String[] args) {
+        AsapJobsClass4 obj;
+        System.out.println(obj.x);
+    }
+} //End of overall class file
