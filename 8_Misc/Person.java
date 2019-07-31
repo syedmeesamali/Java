a289791198c65969c3739196bbb43d90d8d3839e
syedmeesamali <syedmeesamali@yahoo.com>
2019-07-31 23:49:34 +0400

    Java course practice

---
+++ 8_Misc/Person.java
@@ -0,0 +1,23 @@
+class Person {
+   //Fields
+   String name;
+   int age;
+   
+   //Constructor method
+   Person(string name) {
+      this.name = name;
+      age = 0;
+
+   //Methods for class
+   String getName() {
+   return name;
+   }
+
+   void birthday() {
+      age = age + 1;
+      System.out.println("Happy birthday");
+   }
+
+
+} //End of class
+
