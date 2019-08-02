5e01cb72d565266a33a57498bc8f643f307408fc
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-03 00:31:10 +0400

    Preparing for unit testing in eclipse

--- 8_Misc/BankAccount.java
+++ 8_Misc/BankAccount.java
@@ -1,9 +1,39 @@
 //Code for testing in development
-
 public class BankAccount {
    public static void main(String[] args) {      
       double balance;
       String accountOwner;
-      System.out.println("Testing Ended");
+      System.out.println("End of main");
+   }
+   
+   //Method deposit
+   public void deposit(double amount) {
+      //Nothing here yet
+   }
+
+   public void withdraw(double amount) {
+      //Nothing here yet
+   }
+
+
+   public class counter 
+   {
+      int count = 0;
+      public int increment () 
+      {
+         count += 1;
+         return count;
+      }
+
+      public int decrement()
+      {
+         count -= 1;
+         return count;
+      }
+
+      public int getCount()
+      {
+         return count;
+      }
    }
 }
