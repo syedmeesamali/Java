110069fc12c5d0cbf96625785d39f5f899a95496
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-02 23:57:26 +0400

    about test driven devops

---
+++ 8_Misc/BankAccount.java
@@ -0,0 +1,9 @@
+//Code for testing in development
+
+public class BankAccount {
+   public static void main(String[] args) {      
+      double balance;
+      String accountOwner;
+      System.out.println("Testing Ended");
+   }
+}
