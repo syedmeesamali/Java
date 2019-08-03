a4c62ea7a319f4700f4c2dcfb3acc86449f8d996
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-04 00:05:35 +0400

    Full loan program working perfect now

--- 8_Misc/Loan.java
+++ 8_Misc/Loan.java
@@ -1,7 +1,7 @@
 import java.util.Date;
 
 public class Loan {
-   private double loanInterestRate;
+   private double annualInterestRate;
    private int numberOfYears;
    private double loanAmount;
    private java.util.Date loanDate;
@@ -12,12 +12,12 @@ public class Loan {
       this(2.5, 1, 1000); //Put some default values
    }
 
-   public Loan(double loanInterestRate, int numberOfYears, double loanAmount) 
+   public Loan(double annualInterestRate, int numberOfYears, double loanAmount) 
    {
-      this.loanInterestRate = loanInterestRate;
+      this.annualInterestRate = annualInterestRate;
       this.numberOfYears = numberOfYears;
       this.loanAmount = loanAmount;
-      loanDate = java.util.Date();
+      loanDate = new java.util.Date();
    }
 
    public double getAnnualInterestRate() 
@@ -40,12 +40,19 @@ public class Loan {
       this.numberOfYears = numberOfYears;
    }
 
-   public void setLoanAmount(double loanAmount)
+   public void setLoanAmount(double loanAmount) //Set total loan amount
    {
       this.loanAmount = loanAmount;
    }
 
-   public void getMonthlyPayment() 
+   public double getMonthlyPayment()  //Monthly payment amount
+   {
+      double monthlyInterestRate = annualInterestRate / 1200;
+      double monthlyPayment = loanAmount * monthlyInterestRate / (1 - (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
+      return monthlyPayment;
+   }
+   
+   public double getTotalPayment()  //Total payment amount
    {
       double totalPayment = getMonthlyPayment() * numberOfYears * 12;
       return totalPayment;
--- 8_Misc/TestLoan.java
+++ 8_Misc/TestLoan.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-import java.util.scanner;
 
 //Code for testing in development
 public class TestLoan {
    public static void main(String[] args) {      
       Scanner input = new Scanner(System.in);
-      Sysetm.out.print("Enter interest rate e.g. 8.5: ");
+      System.out.print("Enter interest rate e.g. 8.5: ");
       double annualInterestRate = input.nextDouble();
-      Sysetm.out.print("Enter number of years as an integer: ");
+      System.out.print("Enter number of years as an integer: ");
       int numberOfYears = input.nextInt();
 
-      Sysetm.out.print("Enter loan amount e.g. 120000.80: ");
+      System.out.print("Enter loan amount e.g. 120000.80: ");
       double loanAmount = input.nextDouble();
 
       Loan loan = new Loan(annualInterestRate, numberOfYears, loanAmount);
