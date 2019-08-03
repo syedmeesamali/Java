66a83813db396b940a67d1c09b92d8b7d695be9e
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-03 23:55:21 +0400

    Test program for loan class

---
+++ 8_Misc/TestLoan.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.scanner;
+
+//Code for testing in development
+public class TestLoan {
+   public static void main(String[] args) {      
+      Scanner input = new Scanner(System.in);
+      Sysetm.out.print("Enter interest rate e.g. 8.5: ");
+      double annualInterestRate = input.nextDouble();
+      Sysetm.out.print("Enter number of years as an integer: ");
+      int numberOfYears = input.nextInt();
+
+      Sysetm.out.print("Enter loan amount e.g. 120000.80: ");
+      double loanAmount = input.nextDouble();
+
+      Loan loan = new Loan(annualInterestRate, numberOfYears, loanAmount);
+      System.out.printf("The loan was created on %s\n Monthly payment is: %.2f\n", loan.getLoanDate().toString(), 
+         loan.getMonthlyPayment(), loan.getTotalPayment());
+   } //End of main method
+} //End of class
