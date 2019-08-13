dc01cb840c53335114c48247e079ef06de7020e5
syedmeesamali <syedmeesamali@yahoo.com>
2019-08-13 04:05:43 +0400

    Arraylist review in java

---
+++ 8_Misc/testingClass.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class testingClass {
+   
+   public static void main(String[] args)
+   {
+      public Card {
+         char suit;
+         int rank;
+        }
+      
+      ArrayList<Card> deck = new ArrayList<Card>();
+      suits = new char[] {'a', 'b', 'c', 'd', 'f'};
+         for (int i=1; i<=13; i++) {
+            Card c = new Card();
+               for (int j = 0; j<=5; j++) {
+                  c.suit = suits[j];
+                  c.rank = i;
+                  deck.add(c);
+                 System.out.print("suit: " + suits[j] + ", rank: " + i);
+                 }// end of j loop
+            } // End of i loop
+   } //End of main
+   
+} //End of class
+
+  
